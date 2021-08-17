893360701dfb13596c08396bb3340d93b82f9bdf
Patrick Sheridan <sheridp2@gmail.com>
2021-08-16 22:16:34 -0700

    react context drink search app

---
+++ cocktail-finder/src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+        pellentesque, massa vel egestas accumsan, dui tortor egestas dui, at
+        aliquet turpis diam nec erat. Proin vitae elementum dui, ac viverra
+        eros. Nullam nec dui mollis, faucibus orci a, sollicitudin ante. Sed
+        scelerisque quis metus ac suscipit. Suspendisse in odio placerat,
+        interdum ex nec, tristique risus. Sed nec nibh sit amet massa tincidunt
+        pharetra. Mauris neque velit, interdum et lectus nec, tincidunt lobortis
+        velit. Vivamus pellentesque luctus commodo. Nullam nisi orci, interdum
+        vitae consequat at, pretium at sem. Nulla est purus, ultrices in commodo
+        vel, commodo a odio. Vivamus semper justo ut tempor aliquet. Integer et
+        pretium ex. Aenean urna lorem, pulvinar sit amet tincidunt sed, iaculis
+        a arcu. Praesent porta egestas tortor id lacinia. Aliquam accumsan nisi
+        vitae felis vulputate, sed cursus magna feugiat. Praesent mollis risus
+        felis, aliquet faucibus tortor euismod vel. Morbi bibendum libero ac
+        lectus ornare porttitor. Mauris cursus magna lacus, non pretium arcu
+        malesuada at. Phasellus vitae nulla dictum, auctor dolor scelerisque,
+        ultrices leo. Duis volutpat bibendum imperdiet. Nunc a nisl ut leo
+        consequat tincidunt id eu ex.
+      </p>
+    </section>
+  );
+}
---
+++ cocktail-finder/src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Error() {
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>You've reached a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </section>
+  );
+}
